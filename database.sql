7b2d2e7242d49197c55a758dcdb4dd168990c41a
Anton Arkhipov <antonolegovich44@yandex.ru>
2025-01-05 16:53:54 +0300

    step 4

--- database.sql
+++ database.sql
@@ -3,3 +3,14 @@ CREATE TABLE urls (
   name varchar(255) NOT NULL,
   created_at TIMESTAMP NOT NULL
 );
+
+
+CREATE TABLE url_checks (
+  id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  url_id bigint NOT NULL,
+  status_code integer NOT NULL,
+  h1 varchar(255),
+  title varchar(255),
+  description varchar(255),
+  created_at TIMESTAMP NOT NULL
+);
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,6 @@
 import os
 import secrets
 from datetime import datetime
-from unittest import result
 from urllib.parse import urlparse
 
 import psycopg2
@@ -26,25 +25,24 @@ def index():
 @app.post("/")
 def new_url():
     url = request.form.get("url")
-    url_parse = urlparse(url)
+    name_url = get_name_url(url)
 
     if not validate_url(url):
         flash("Некорректная ссылка", category="error")
         return redirect(url_for("index", url=url))
 
-    hostname = url_parse.hostname
     conn = psycopg2.connect(DATABASE_URL)
     query_name = "SELECT id, name FROM urls WHERE name = %s"
 
     with conn.cursor() as cursor:
-        cursor.execute(query_name, (hostname,))
+        cursor.execute(query_name, (name_url,))
         result = cursor.fetchone()
         if result is None:
             add_url = """INSERT INTO
                             urls (name, created_at)
                          VALUES
                             (%s, %s) RETURNING id"""
-            cursor.execute(add_url, (hostname, datetime.now()))
+            cursor.execute(add_url, (name_url, datetime.now()))
             url_id = cursor.fetchone()[0]
         else:
             url_id = result[0]
@@ -57,6 +55,13 @@ def new_url():
     return redirect(url_for("get_url", url_id=url_id))
 
 
+def get_name_url(url):
+    url_parse = urlparse(url)
+    scheme = url_parse.scheme
+    hostname = url_parse.netloc
+    return f"{scheme}://{hostname}"
+
+
 def validate_url(url):
     return len(url) <= 255 and validators.url(url)
 
@@ -65,19 +70,48 @@ def validate_url(url):
 def get_url(url_id):
     messages = get_flashed_messages(with_categories=True)
     conn = psycopg2.connect(DATABASE_URL)
-    query = "SELECT id, name, created_at FROM urls WHERE id = %s"
+    query = "SELECT id, name FROM urls WHERE id = %s"
 
     with conn.cursor(cursor_factory=RealDictCursor) as cursor:
         cursor.execute(query, (url_id,))
         url = cursor.fetchone()
 
+    with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+        query = "SELECT id, status_code, h1, title, description, created_at FROM url_checks WHERE url_id = %s ORDER BY created_at DESC"
+        cursor.execute(query, (url_id,))
+        check_url = cursor.fetchall()
+
     conn.close()
-    return render_template("url.html", url=url, messages=messages)
+
+    return render_template("url.html", url=url, check_url=check_url, messages=messages)
+
+
+@app.post("/urls/<int:url_id>/checks")
+def new_check(url_id):
+    conn = psycopg2.connect(DATABASE_URL)
+    with conn.cursor() as cursor:
+        query = """INSERT INTO
+                       url_checks (url_id, status_code, h1, title, description, created_at)
+                   VALUES
+                       (%s, %s, %s, %s, %s, %s)"""
+        cursor.execute(query, (url_id, 000, "test", "test", "test", datetime.now()))
+
+    conn.commit()
+    flash("Страница успешно проверена", category="success")
+    return redirect(url_for("get_url", url_id=url_id))
 
 
 @app.route("/urls")
-def get_urls():
-    query = "SELECT id, name, created_at FROM urls ORDER BY created_at DESC"
+def get_all_urls():
+    query = """SELECT
+                   urls.id,
+                   urls.name,
+                   urls.created_at,
+                   MAX(url_checks.created_at) as last_check
+               FROM urls
+               LEFT JOIN url_checks ON urls.id = url_checks.url_id
+               GROUP BY urls.id
+               ORDER BY created_at DESC"""
     conn = psycopg2.connect(DATABASE_URL)
 
     with conn.cursor(cursor_factory=RealDictCursor) as cursor:
