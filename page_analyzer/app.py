d584eef76771737c6290029c0c884e8b9ae777b6
Anton Arkhipov <antonolegovich44@yandex.ru>
2025-01-08 11:26:30 +0300

    implementing a context manager as a class

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,10 +1,10 @@
-import secrets
+import os
 
+from dotenv import load_dotenv
 from flask import (
     Flask,
     flash,
     get_flashed_messages,
-    make_response,
     redirect,
     render_template,
     request,
@@ -13,13 +13,16 @@ from flask import (
 
 from page_analyzer import db_manager, utils
 
+load_dotenv()
 app = Flask(__name__)
-app.secret_key = secrets.token_hex(16)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+urls_repo = db_manager.UrlsRepository(DATABASE_URL)
 
 
 @app.route("/")
 def index():
-    # messages = get_flashed_messages(with_categories=True)
     url = request.args.get("url", "")
     return render_template("index.html", url=url)
 
@@ -47,8 +50,8 @@ def index():
 @app.route("/urls/<int:url_id>")
 def get_url(url_id):
     messages = get_flashed_messages(with_categories=True)
-    url = db_manager.get_url(url_id)
-    checks_url = db_manager.get_all_url_checks(url_id)
+    url = urls_repo.get_url(url_id)
+    checks_url = urls_repo.get_all_url_checks(url_id)
     return render_template(
         "url.html", url=url, checks_url=checks_url, messages=messages
     )
@@ -56,12 +59,12 @@ def get_url(url_id):
 
 @app.post("/urls/<int:url_id>/checks")
 def new_check(url_id):
-    url = db_manager.get_url(url_id)["name"]
+    url = urls_repo.get_url(url_id)["name"]
     status_code = utils.get_status_code(url)
 
     if status_code:
         tags = utils.get_tags_url(url)
-        db_manager.save_url_check(url_id, status_code, tags)
+        urls_repo.save_url_check(url_id, status_code, tags)
         flash("Страница успешно проверена", category="success")
         return redirect(url_for("get_url", url_id=url_id))
     else:
@@ -79,21 +82,16 @@ def get_all_urls():
         if not utils.validate_url(name_url):
             flash("Некорректный URL", category="error")
             messages = get_flashed_messages(with_categories=True)
-            # return render_template("index.html", url=url, messages=messages)
-            response = make_response(
-                render_template("index.html", url=url, messages=messages)
-            )
-            response.status_code = 422
-            return response
+            return render_template("index.html", url=url, messages=messages), 422
         try:
-            db_manager.save_url(name_url)
-            url_id = db_manager.get_id_url(name_url)
+            urls_repo.save_url(name_url)
+            url_id = urls_repo.get_id_url(name_url)
             flash("Страница успешно добавлена", category="success")
             return redirect(url_for("get_url", url_id=url_id))
         except db_manager.URLError:
-            url_id = db_manager.get_id_url(name_url)
+            url_id = urls_repo.get_id_url(name_url)
             flash("Страница уже существует", category="info")
             return redirect(url_for("get_url", url_id=url_id))
 
-    urls = db_manager.get_all_urls()
+    urls = urls_repo.get_all_urls()
     return render_template("urls.html", urls=urls)
--- page_analyzer/db_manager.py
+++ page_analyzer/db_manager.py
@@ -1,136 +1,108 @@
-import os
 from datetime import datetime
 
 import psycopg2
-from dotenv import load_dotenv
 from psycopg2.extras import RealDictCursor
-
-load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 class URLError(Exception):
     pass
 
 
-def get_connection():
-    return psycopg2.connect(DATABASE_URL)
+class DatabaseConnection:
+    def __init__(self, dsn, cursor_factory=None):
+        self.dsn = dsn
+        self.cursor_factory = cursor_factory
+        self.conn = None
+
+    def __enter__(self):
+        self.conn = psycopg2.connect(self.dsn)
+        self.conn.autocommit = True
+        if self.cursor_factory:
+            return self.conn.cursor(cursor_factory=self.cursor_factory)
+        return self.conn.cursor()
+
+    def __exit__(self, type, value, traceback):
+        if self.conn:
+            self.conn.close()
 
 
-def save_url(url):
-    conn = get_connection()
+class UrlsRepository:
+    def __init__(self, dsn):
+        self.dsn = dsn
 
-    with conn.cursor() as cursor:
-        query = "SELECT id, name FROM urls WHERE name = %s"
-        cursor.execute(query, (url,))
-        result = cursor.fetchone()
+    def save_url(self, url):
+        with DatabaseConnection(self.dsn) as cursor:
+            query = "SELECT id, name FROM urls WHERE name = %s"
+            cursor.execute(query, (url,))
+            result = cursor.fetchone()
+            if result is None:
+                add_url = """INSERT INTO urls (name, created_at)
+                             VALUES (%s, %s)"""
+                cursor.execute(add_url, (url, datetime.now()))
+            else:
+                raise URLError()
 
-        if result is None:
-            add_url = """INSERT INTO urls (name, created_at)
-                         VALUES (%s, %s)"""
-            cursor.execute(add_url, (url, datetime.now()))
+    def get_url(self, url_id):
+        with DatabaseConnection(self.dsn, cursor_factory=RealDictCursor) as cursor:
+            query = "SELECT id, name, created_at FROM urls WHERE id = %s"
+            cursor.execute(query, (url_id,))
+            url = cursor.fetchone()
+        return url
 
-        else:
-            raise URLError()
+    def get_id_url(self, url):
+        with DatabaseConnection(self.dsn) as cursor:
+            query = "SELECT id FROM urls WHERE name = %s"
+            cursor.execute(query, (url,))
+            result = cursor.fetchone()
+            url_id = result[0]
+        return url_id
 
-    conn.commit()
-    conn.close()
+    def get_all_urls(self):
+        with DatabaseConnection(self.dsn, cursor_factory=RealDictCursor) as cursor:
+            query = """SELECT
+                           urls.id,
+                           urls.name,
+                           urls.created_at,
+                           MAX(url_checks.created_at) as last_check,
+                           url_checks.status_code
+                       FROM urls
+                       LEFT JOIN url_checks ON urls.id = url_checks.url_id
+                       GROUP BY urls.id, url_checks.status_code
+                       ORDER BY created_at DESC"""
+            cursor.execute(query)
+            urls = cursor.fetchall()
+        return urls
 
+    def save_url_check(self, url_id, status_code, tags):
+        with DatabaseConnection(self.dsn) as cursor:
+            query = """INSERT INTO url_checks (
+                        url_id, status_code, h1, title, description, created_at
+                        )
+                       VALUES (%s, %s, %s, %s, %s, %s)"""
+            cursor.execute(
+                query,
+                (
+                    url_id,
+                    status_code,
+                    tags["h1"],
+                    tags["title"],
+                    tags["description"],
+                    datetime.now(),
+                ),
+            )
 
-def get_url(url_id):
-    conn = psycopg2.connect(DATABASE_URL)
-
-    with conn.cursor(cursor_factory=RealDictCursor) as cursor:
-        query = "SELECT id, name, created_at FROM urls WHERE id = %s"
-        cursor.execute(query, (url_id,))
-        url = cursor.fetchone()
-
-    conn.commit()
-    conn.close()
-
-    return url
-
-
-def get_id_url(url):
-    conn = get_connection()
-
-    with conn.cursor() as cursor:
-        query = "SELECT id FROM urls WHERE name = %s"
-        cursor.execute(query, (url,))
-        result = cursor.fetchone()
-        url_id = result[0]
-
-    conn.commit()
-    conn.close()
-
-    return url_id
-
-
-def get_all_urls():
-    conn = get_connection()
-
-    with conn.cursor(cursor_factory=RealDictCursor) as cursor:
-        query = """SELECT
-                       urls.id,
-                       urls.name,
-                       urls.created_at,
-                       MAX(url_checks.created_at) as last_check,
-                       url_checks.status_code
-                   FROM urls
-                   LEFT JOIN url_checks ON urls.id = url_checks.url_id
-                   GROUP BY urls.id, url_checks.status_code
-                   ORDER BY created_at DESC"""
-        cursor.execute(query)
-        urls = cursor.fetchall()
-
-    conn.commit()
-    conn.close()
-
-    return urls
-
-
-def save_url_check(url_id, status_code, tags):
-    conn = get_connection()
-
-    with conn.cursor() as cursor:
-        query = """INSERT INTO url_checks (
-                       url_id, status_code, h1, title, description, created_at
-                       )
-                   VALUES (%s, %s, %s, %s, %s, %s)"""
-        cursor.execute(
-            query,
-            (
-                url_id,
-                status_code,
-                tags["h1"],
-                tags["title"],
-                tags["description"],
-                datetime.now(),
-            ),
-        )
-
-    conn.commit()
-    conn.close()
-
-
-def get_all_url_checks(url_id):
-    conn = get_connection()
-
-    with conn.cursor(cursor_factory=RealDictCursor) as cursor:
-        query = """SELECT
-                       id,
-                       status_code,
-                       h1,
-                       title,
-                       description,
-                       created_at
-                   FROM url_checks
-                   WHERE url_id = %s
-                   ORDER BY created_at DESC"""
-        cursor.execute(query, (url_id,))
-        check_url = cursor.fetchall()
-
-    conn.commit()
-    conn.close()
-
-    return check_url
+    def get_all_url_checks(self, url_id):
+        with DatabaseConnection(self.dsn, cursor_factory=RealDictCursor) as cursor:
+            query = """SELECT
+                           id,
+                           status_code,
+                           h1,
+                           title,
+                           description,
+                           created_at
+                       FROM url_checks
+                       WHERE url_id = %s
+                       ORDER BY created_at DESC"""
+            cursor.execute(query, (url_id,))
+            check_url = cursor.fetchall()
+        return check_url
